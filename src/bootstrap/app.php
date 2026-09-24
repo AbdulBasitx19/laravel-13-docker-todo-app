6d9d9e3cd2ddedca43eed20e5ef1687a0a65b444
Abdul Basit <abdulbasitchaudhry106@gmail.com>
2026-09-24 19:19:26 +0500

    Configured dockerized ngrok container with secure root env

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -13,6 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        // ngrok domain headers ko trust karne ke liye
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
